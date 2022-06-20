13365c3c754465dfee32a451d6cccd01ce54bf23
Thiago Gomes <tsg.gomes9@gmail.com>
2022-06-20 11:46:59 -0400

    Adição do input senha

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,6 +5,7 @@ export function Home() {
     <div className = "container">
       <h1>Seja bem-vindo!</h1>
       <input type="text" placeholder="Informe seu código de usuário"/>
+      <input type="password" placeholder="Digite sua senha"/>
       <button>Entrar na conta</button>
     </div>
     
