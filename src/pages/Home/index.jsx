2b2c2f4d18ff2be02953b7111e0d2f20c98fe36a
Thiago Gomes <tsg.gomes9@gmail.com>
2022-06-20 11:44:21 -0400

    Versão inicial da Tela de Login

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+export function Home() {
+  return (
+    <div className = "container">
+      <h1>Seja bem-vindo!</h1>
+      <input type="text" placeholder="Informe seu código de usuário"/>
+      <button>Entrar na conta</button>
+    </div>
+    
+  )
+}
